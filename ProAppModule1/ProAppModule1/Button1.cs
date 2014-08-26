c5bb49d00b63961126c9b705e40be45ac142bec5
Shriram Bhutada <sbhutada@esri.com>
2014-08-26 08:58:09 -0400

    ArcGIS Pro first sample
    This project is a sample code for accessing various containers in
    ArcGISPro like Map, GP etc and how to loop through the project items in
    the containers

---
+++ ProAppModule1/ProAppModule1/Button1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Catalog;
+
+namespace ProAppModule1
+{
+    internal class Button1 : Button
+    {
+        protected override void OnClick()
+        {
+            Project m_Project = ArcGIS.Desktop.Core.ProjectModule.CurrentProject;
+            
+
+            ArcGIS.Desktop.Mapping.MapContainer mapContainer = m_Project.GetProjectItemContainer("Map") as ArcGIS.Desktop.Mapping.MapContainer;
+            ArcGIS.Desktop.GeoProcessing.GeoprocessingContainer m_GeoprocessingContainer = m_Project.GetProjectItemContainer("GP") as ArcGIS.Desktop.GeoProcessing.GeoprocessingContainer;
+            ArcGIS.Desktop.Catalog.GDBContainer m_GDBContainer = m_Project.GetProjectItemContainer("GDB") as ArcGIS.Desktop.Catalog.GDBContainer;
+            ArcGIS.Desktop.Catalog.FolderConnectionContainer m_FolderConnectionContainer = m_Project.GetProjectItemContainer("FolderConnection") as ArcGIS.Desktop.Catalog.FolderConnectionContainer;
+            ArcGIS.Desktop.Core.DisplayUnitEnvironmentItemContainer m_DisplayUnitEnvironmentItemContainer = m_Project.GetProjectItemContainer("DisplayUnitEnvironment") as ArcGIS.Desktop.Core.DisplayUnitEnvironmentItemContainer;
+            ArcGIS.Desktop.Mapping.StyleContainer m_StyleContainer = m_Project.GetProjectItemContainer("Styles") as ArcGIS.Desktop.Mapping.StyleContainer;
+            
+           
+
+            foreach (MapProjectItem item in mapContainer.GetProjectItems())
+            {
+            
+               // ArcGIS.Desktop.Internal.Framework.Metro.MessageBox.Show(item.Name);
+
+            }
+
+            foreach (FolderConnectionProjectItem folderitem in m_FolderConnectionContainer.GetProjectItems())
+            {
+
+                ArcGIS.Desktop.Internal.Framework.Metro.MessageBox.Show(folderitem.Name);
+            }
+        }
+    }
+}
